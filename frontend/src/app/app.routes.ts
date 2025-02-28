1fd97f69bd8139b80480e1d26e20956b41c838a0
alvarobahos <alvarobahosc@gmail.com>
2025-02-27 21:34:00 -0500

    advance

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { PagenotfoundComponent } from './components/pages/pagenotfound/pagenotfound.component';
+
+export const routes: Routes = [
+  { path: 'inicio', title: 'SPA | Inicio', component: HomeComponent },
+  { path: 'prodcutos', title: 'SPA | Productos', component: ProductsComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  {
+    path: '**',
+    title: 'Pagina No Encontrada',
+    component: PagenotfoundComponent,
+  },
+];
